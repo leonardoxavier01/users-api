6d27409786df91a191456fa7cb67cae1924bd4e7
unknown <leonardoxavier092@gmail.com>
2022-04-19 13:24:27 -0300

    test: method post

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,12 +4,14 @@ const { join } = require("path");
 const filePath = join(__dirname, "users.json");
 
 const getUsers = () => {
-  const data = fs.existsSync(filePath) ? fs.readFileSync(filePath) : [];
+  const data = fs.existsSync(filePath) 
+  ? fs.readFileSync(filePath) 
+  : [];
 
   try {
-    return JSON.parse(data);
+     return JSON.parse(data);
   } catch (error) {
-    return [];
+     return [];
   }
 };
 
@@ -17,10 +19,23 @@ const saveUser = (users) =>
   fs.writeFileSync(filePath, JSON.stringify(users, null, "\t"));
 
 const userRoute = (app) => {
-  app.route("/users/:id?").get((req, res) => {
-    const users = getUsers();
-    res.send({ users });
-  });
+  app.route("/users/:id?")
+    .get((req, res) => {
+      const users = getUsers();
+
+       res.send({ users });
+    });
+    .post((req, res) => {
+      const users = getUsers();
+      
+       users.push(req.body)
+       saveUser(users)
+
+       res.send(201).send('OK')
+    });
+    
 };
+
+
 
 module.exports = userRoute;
